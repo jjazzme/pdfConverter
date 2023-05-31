457f57d2bb01e30650a660368e5782098e1f2a99
me <me@don.ru>
2023-05-31 08:13:42 +0000

    1.0.0

--- p74/www/index.php
+++ p74/www/index.php
@@ -9,22 +9,33 @@ $template->setImageValue('podpisantPodpis', [
     'height' => '200px',
     'ratio' => true
 ]);
-$stream = fopen('php://temp', 'r+');
-$template->save($stream);
-rewind($stream);
-header('Content-Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document');
-header('Content-Disposition: inline; filename="output.docx"');
-fpassthru($stream);
-fclose($stream);
-exit;
 
-//$targetPath = tempnam(sys_get_temp_dir(), 'phpword_');
-//$template->saveAs($targetPath);
-//if (file_exists($targetPath)) {
-//    header('Content-Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document');
-//    header('Content-Disposition: inline; filename="output.pdf"');
-//    readfile($targetPath);
-//    exit;
-//} else {
-//    echo 'conversion error';
-//}
+
+$regex = '/^phpword_.+$/';
+$timestamp = time() - 300;
+$files = scandir(sys_get_temp_dir());
+foreach ($files as $file) {
+    if (preg_match($regex, $file)) {
+        $filePath = sys_get_temp_dir() . '/' . $file;
+        $fileCreationTime = filectime($filePath);
+        if ($fileCreationTime <= $timestamp) {
+            unlink($filePath);
+        }
+    }
+}
+
+$targetPath = tempnam(sys_get_temp_dir(), 'phpword_');
+$template->saveAs($targetPath);
+
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, 'http://p73');
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_POST, true);
+$fileData = curl_file_create($targetPath, mime_content_type($targetPath), basename($targetPath));
+$postData = array('file' => $fileData);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $postData);
+$response = curl_exec($curl);
+curl_close($curl);
+header('Content-Type: application/pdf');
+header('Content-Disposition: inline; filename="output.pdf"');
+echo $response;
